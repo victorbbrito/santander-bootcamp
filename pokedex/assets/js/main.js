8b544c31750b085fe2f85e2d7e5833aea8de0908
Victor Brito <victorbbrito@hotmail.com>
2023-09-01 23:51:59 -0400

    pokedex done

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,36 @@
-console.log("sucess")
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+let offset = 0
+const limit = 12
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon ${pokemon.mainType}">
+            <span class="number">#${pokemon.id}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+
+        </li>
+    `
+}
+
+function loadPokemonItens(offset,limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    })
+}
+
+loadPokemonItens(offset,limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset,limit)
+})
+
